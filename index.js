c009eaba2214abe2ae541318419133e356e13cdf
Rajav Jain <98579217+RajavJain@users.noreply.github.com>
2022-10-27 22:58:19 +0530

    Updated Comments

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ mongoose.connect("mongodb://localhost:27017/SampleWork").then(()=>{ //ye cmd lin
     console.log(err);
 })
 
-const student= mongoose.Schema({ //ye ek type type ka blueprint bn jaaega, jiska naam student rakh diya hai
+const student= mongoose.Schema({ //ye ek type type ka blueprint bn jaaega, jiska naam student rakh diya hai and usme data store krne k type mention kr diye hai
     name: String,
     class: String,
     roll_no : Number
 })
  
-const Student= mongoose.model("Student", student);//it's model or a collection where we can store data
+const Student= mongoose.model("Student", student); //it's model or a collection where we can store data
 
 //METHOD I :-
 // const addData = async () =>{
@@ -29,13 +29,14 @@ const Student= mongoose.model("Student", student);//it's model or a collection w
 
 //METHOD II :-
 const addData1 = async()=>{
-
     const s1 = await Student.create({//used create method, its a basically good approach
         name: "Babu bhaiya",
         class: "Tenth",
         roll_no: "09"
     })
-}//Similarly we have find and other method to do CRUD operations in it.
+}
+
+//Similarly we have find and other method to do CRUD operations in it.
 
 addData1();
 
